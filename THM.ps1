389245e23c4dea2e36cdf11205ea15042d78a24b
Kilgharrah68 <143011600+Kilgharrah68@users.noreply.github.com>
2024-05-13 21:18:37 +0100

    Update THM.ps1

--- THM.ps1
+++ THM.ps1
@@ -1,4 +1,4 @@
-﻿#
+#
 # THM: Temperature Hash Monitor
 #
 
@@ -7,57 +7,81 @@
 $configFile = "config.txt"
 $configData = Get-Content $configFile
 
-# Parse data from the configuration file and assign values to variables
+# Parse configuration data from file
 foreach ($line in $configData) {
-  $key, $value = $line.Split('=').Trim()
-  switch ($key) {
-    "token" { $token = $value }
-    "chat_id" { $chat_id = $value }
-    "num_miners" { $num_miners = $value }
-    "wait_time_seconds" { $wait_time_seconds = $value }
-    "min_temp" { $min_temp = $value }
-    "max_temp" { $max_temp = $value }
-    "min_hashrate" { $min_hashrate = [int]$value }
-  }
+    $key, $value = $line.Split('=').Trim()
+    switch ($key) {
+        "apikey" { $apikey = $value }
+        "phone" { $phone = $value }
+        "token" { $token = $value }
+        "chat_id" { $chat_id = $value }
+        "num_miners" { $num_miners = $value }
+        "wait_time_seconds" { $wait_time_seconds = $value }
+        "min_temp" { $min_temp = $value }
+        "max_temp" { $max_temp = $value }
+        "min_hashrate" { $min_hashrate = [int]$value }
+    }
 }
 
-# Define function to send message to Telegram
-function SendMessageTelegram($message) {
+# Function to send message via WhatsApp
+function SendMessageWhatsapp($message) {
     try {
-        # Get current date and time
         $currentDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    
-        # Add date, time, and remote IP to message
-
         $messageWithDateTime = "$ip_miner - $currentDateTime - $message"
-         # Construct Telegram API URL
-        $url = "https://api.telegram.org/bot$token/sendMessage"
+        $url = "https://api.callmebot.com/whatsapp.php?phone=$phone&text=$messageWithDateTime&apikey=$apikey"
+       
         $body = @{
             chat_id = $chat_id
             text = $messageWithDateTime
             parse_mode = "HTML"
         }
 
-        # Send message to Telegram
+        # Invoke REST API
+        $response = Invoke-RestMethod -Uri $url -Method Post -Body $body
+
+        if ($response -match "You will receive it in a few seconds") {
+            Write-Host "Message sent successfully to WhatsApp."
+            WriteToLog $ip_miner $tempValue $hashRateValue
+        } else {
+            Write-Host "Error sending message to WhatsApp: $response"
+            WriteToLog "Error sending message to WhatsApp: $response"
+        }
+    } catch {
+        Write-Host "Error sending message to WhatsApp: $_"
+        WriteToLog "Error sending message to WhatsApp: $_"
+    }
+}
+
+# Function to send message via Telegram
+function SendMessageTelegram($message) {
+    try {
+        $currentDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $messageWithDateTime = "$ip_miner - $currentDateTime - $message"
+        $url = "https://api.telegram.org/bot$token/sendMessage"
+
+        $body = @{
+            chat_id = $chat_id
+            text = $messageWithDateTime
+            parse_mode = "HTML"
+        }
+
+        # Invoke REST API
         $response = Invoke-RestMethod -Uri $url -Method Post -Body $body
 
         if ($response.ok) {
             Write-Host "Message sent successfully to Telegram."
-            # Write to log file
             WriteToLog $ip_miner $tempValue $hashRateValue
         } else {
             Write-Host "Error sending message to Telegram: $($response.error_code) - $($response.description)"
-            # Write to log file
             WriteToLog "Error sending message to Telegram: $($response.error_code) - $($response.description)"
         }
-
     } catch {
         Write-Host "Error sending message to Telegram: $_"
-        # Write to log file
         WriteToLog "Error sending message to Telegram: $_"
     }
 }
 
+# Function to write to log file
 function WriteToLog($ip_miner, $tempValue, $hashRateValue) {
     $currentDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $messageWithDateTime = "$ip_miner - $currentDateTime - Temp: $tempValue, HashRate: $hashRateValue"
@@ -66,23 +90,18 @@ function WriteToLog($ip_miner, $tempValue, $hashRateValue) {
 
 # Function to search for Bitaxe miners on the local network
 function SearchForBitaxeMiners {
-    # Get IP addresses assigned by the router
     $arpOutput = arp -a | Where-Object { $_ -match '192.168.1' } | ForEach-Object { ($_ -split '\s+')[1] }
     $ipAddresses = $arpOutput -split '\s+'
 
     # Define a list to store identified Bitaxe miners
     $bitaxeMiners = New-Object System.Collections.ArrayList
-
-    # Wait time in seconds (adjustable as needed)
     $waitTime = 0.6
 
-    # Iterate through each IP address assigned by the router
     foreach ($ipAddress in $ipAddresses) {
         CheckBitaxeMiner $ipAddress
 
-        # Check if the desired number of Bitaxe devices has been reached
         if ($bitaxeMiners.Count -ge $num_miners) {
-            break  # Exit the loop if enough devices have been found
+            break
         }
     }
 
@@ -93,31 +112,23 @@ function SearchForBitaxeMiners {
     return $bitaxeMiners
 }
 
+# Function to check Bitaxe miner status
 function CheckBitaxeMiner([string]$ipAddress) {
-        try {
-            # Construct API URL for the current IP address
-            $apiUrl = "http://$ipAddress/api/system/info"
+    try {
+        $apiUrl = "http://$ipAddress/api/system/info"
+        $apiResponse = Invoke-RestMethod -Uri $apiUrl -Method Get -TimeoutSec $waitTime -ErrorAction Stop
 
-            # Invoke REST API call with timeout
-            $apiResponse = Invoke-RestMethod -Uri $apiUrl -Method Get -TimeoutSec $waitTime -ErrorAction Stop
+        if ($apiResponse -match '"ASICModel"\s*:\s*"BM.+?"') {
+            [void]$bitaxeMiners.Add($ipAddress)
 
-            # Check if the response contains ASICModel and capture its value
-            if ($apiResponse -match '"ASICModel"\s*:\s*"BM.+?"') {
-                # Add the current IP address to the list of identified Bitaxe miners
-                [void]$bitaxeMiners.Add($ipAddress)
-                Write-Output $ipAddress  # Only return the IP address
-
-                # Check if the desired number of Bitaxe devices has been reached
-                if ($bitaxeMiners.Count -ge $num_miners) {
-                    return  # Exit the function if enough devices have been found
-                }
+            if ($bitaxeMiners.Count -ge $num_miners) {
+                return
             }
-
-        } catch {
-            # Handle any exceptions that occur during API calls
-            # Write-Warning "Error connecting to IP: $ipAddress - Error: $($_.Exception.Message)"
         }
+    } catch {
+        # Handle any exceptions that occur during API calls
     }
+}
 
 # Initial search for Bitaxe miners
 Write-Output "Searching for miners..."
@@ -128,12 +139,9 @@ while ($true) {
     foreach ($ip_miner in $bitaxeMiners) {
         $successful_connection = $false
         try {
-            # Attempt to retrieve system data from the URL
             $systemInfo = Invoke-RestMethod -Uri "$ip_miner/api/system/info" -TimeoutSec 120
-            # Mark connection as successful if no errors occur while getting data
             $successful_connection = $true
 
-            # Get "temp" and "hashRate" values directly from JSON string
             if ($systemInfo -match '"temp":\s*(\d+)') {
                 $tempValue = [int]$matches[1]
             }
@@ -142,40 +150,39 @@ while ($true) {
                 $hashRateValue = [int]$matches[1]
             }
 
-            # Determine conditions for sending message to Telegram
-            $message = ""
+            $messageT = ""
+            $messageW = ""
             if ($tempValue -lt $min_temp -or $tempValue -gt $max_temp) {
-                $message += "<b>Temp: $tempValue</b>, HashRate: $hashRateValue"
+                $messageT += "<b>Temp: $tempValue</b>, HashRate: $hashRateValue"
+                $messageW += "_*Temp: $tempValue*_, HashRate: $hashRateValue"
             }
 
             if ($hashRateValue -lt $min_hashrate) {
-                $message += "Temp: $tempValue, <b>HashRate: $hashRateValue</b>"
+                $messageT += "Temp: $tempValue, <b>HashRate: $hashRateValue</b>"
+                $messageW += "Temp: $tempValue, _*HashRate: $hashRateValue*_"
             }
 
-            # Send message to Telegram if any condition is met
-            if (-not [string]::IsNullOrWhiteSpace($message)) {
-                SendMessageTelegram($message)
-                # Search for Bitaxe miners before continuing with the loop
-                Write-Output "Searching for miners..."
-                $bitaxeMiners = SearchForBitaxeMiners
-                Start-Sleep -Seconds (10 * $num_miners)
-            } 
-        } catch {
-            # Display error message only if connection was not successful after 120 seconds
-            if (-not $successful_connection) {
-                Write-Host $ip_miner
-                Write-Host "Error retrieving system data: Unable to connect to remote server."
+            if (($token -ne "" -and $chat_id -ne "") -and (-not [string]::IsNullOrWhiteSpace($messageT))) {
+                SendMessageTelegram($messageT)
             }
+
+            if (($apikey -ne "" -and $phone -ne "") -and (-not [string]::IsNullOrWhiteSpace($messageW))) {
+                SendMessageWhatsapp($messageW)
+            }
+
+            Write-Output "Searching for miners..."
+            $bitaxeMiners = SearchForBitaxeMiners
+            Start-Sleep -Seconds (10 * $num_miners)
+        } catch {
+            # Handle errors
         }
     }
 
-    # Get current date, time, and remote IP for displaying in console
     $currentDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $consoleMessage = "$ip_miner - $currentDateTime - Temp: $tempValue, HashRate: $hashRateValue"
    
     Write-Host $consoleMessage
     WriteToLog $ip_miner $tempValue $hashRateValue
 
-    # Wait for specified time before next check
     Start-Sleep -Seconds $wait_time_seconds
 }
